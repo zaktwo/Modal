62e6a20f960e85121cacfc5e7ba476bc0a348911
Zak Maaz <zaktwo@users.noreply.github.com>
2023-06-25 16:08:41 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.onverlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i=0; i < btnsOpenModal.length; i++){
+    
+}
