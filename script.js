0d7ecfeb662ee1a97a0a08c72858741a482631e3
Zak Maaz <zaktwo@users.noreply.github.com>
2023-06-26 15:53:52 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const overLay = document.querySelector('.onverlay');
+const overLay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+// the classic for loop
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', openModal);
+// }
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-  });
-}
-btnCloseModal.addEventListener('click', e =>{
-    modal.classList.add('hidden');
+// and array forEach loop both are working
+btnsOpenModal.forEach(element => {
+  element.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overLay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
